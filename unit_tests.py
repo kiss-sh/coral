ff8a8e8b5f212c138f18b68e9654aa7eaaf71d63
Marcos Fonseca <marco12.fonseca@hotmail.com>
2021-10-04 00:17:35 -0300

    a classe Token e a func tokenizer recebeu melhorias

--- unit_tests.py
+++ unit_tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from lexer import Source, tokenizer, Toker
+from lexer import Source, tokenizer, Token
 
 class Test(unittest.TestCase):
     def test_lexer_source_function(self):
@@ -13,19 +13,18 @@ class Test(unittest.TestCase):
 
     def test_tokenizer_function(self):
         source = Source('test = 1')
-        token0 = Toker('identifier', value='test')
-        token1 = Toker('equals or assigns')
-        token2 = Toker('integer', value='1')
         tokens = tokenizer(source)
 
         self.assertTrue(len(tokens) == 3)
-        self.assertEqual(token0.type,  tokens[0].type)
-        self.assertEqual(token0.value, tokens[0].value)
-        self.assertEqual(token1.type,  tokens[1].type)
-        self.assertEqual(token1.value, tokens[1].value)
-        self.assertEqual(token2.type,  tokens[2].type)
-        self.assertEqual(token2.value, tokens[2].value)
+        self.assertEqual(Token.IDENTIFIER, tokens[0].type)
+        self.assertEqual('test', tokens[0].value)
 
+        self.assertEqual(Token.EQUAL, tokens[1].type)
+        self.assertEqual(None,  tokens[1].value)
+
+        self.assertEqual(Token.INTEGER, tokens[2].type)
+        self.assertEqual(1, tokens[2].value)
+                                      
         source = Source('#teste')
         tokens = tokenizer(source)
         self.assertTrue(len(tokens) == 0)
