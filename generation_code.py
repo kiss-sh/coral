43aca7bf03bc6ef8a1c457b8fa5fd94a94d61a24
Marcos Fonseca <marco12.fonseca@hotmail.com>
2021-10-06 23:42:14 -0300

    corrigido falta de impresão dos ultimos nodes

--- generation_code.py
+++ generation_code.py
@@ -12,6 +12,8 @@ def ast_to_code(ast):
         code += ' +'
     elif ast.data.type == Token.MULTIPLY:
         code += ' *'
+    else:
+        code += f' {ast.data.value}'
 
     if ast.left is not None:
         code += ast_to_code(ast.left)
