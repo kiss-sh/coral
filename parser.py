05b434cac95ed83726c9b2b5a998fab9e8f25cb9
Marcos Fonseca <marco12.fonseca@hotmail.com>
2021-11-02 23:11:09 -0300

    adicionado comentarios e outras melhorias de estilo

--- parser.py
+++ parser.py
@@ -1,6 +1,10 @@
 from lexer import Token
 
 def replace_keywords(tokens):
+    """
+    substituir palavras chaves da linguagem de origem por
+    instruções equivalentes na linguagem de destino
+    """
     REPLACE_KEYWORDS = {
             'False': 'false',
             'None': 'null',
@@ -15,19 +19,37 @@ def replace_keywords(tokens):
             token.value = REPLACE_KEYWORDS[token.value]
 
 def fix_variable_declaration(tokens):
-    _index = 0
-    while _index < len(tokens):
-        if _index-1 >= 0 and \
-           tokens[_index].type   == Token.EQUAL and \
-           tokens[_index+1].type != Token.EQUAL and \
-           tokens[_index-1].type == Token.IDENTIFIER:
+    """
+    corrige a forma de declaração de variaveis,
+    a logica para corrigir consister em simplesmente
+    adicionar antes um token do tipo indentifier com valor 'var'
+
+    exemplo: teste = 1 -> var teste = 1
+    """
+
+    idx = 0
+    while idx < len(tokens):
+        if idx-1 >= 0 and \
+           tokens[idx].type   == Token.EQUAL and \
+           tokens[idx+1].type != Token.EQUAL and \
+           tokens[idx-1].type == Token.IDENTIFIER:
             var_keyword = Token(Token.IDENTIFIER, value='var')
-            tokens.insert(_index-1, var_keyword)
-            _index += 1
-        _index += 1
+            tokens.insert(idx-1, var_keyword)
+            idx += 1
+        idx += 1
 
 def fix_code_blocks(tokens):
+    """
+    corrige o estilo de codigo usado em if/else e while,
+    esta função contém muitas funções aninhadas reponsaveis
+    por cada etapa da correção
+    """
+
     def fix_header_block(index_keyword, tokens):
+        """
+        corrige o estilo da declaração de codigo,
+        exemplo: while True: -> while(True){
+        """
         tokens.insert(index_keyword+1, Token(Token.OPEN_PARANTHESIS))
 
         idx = index_keyword
@@ -37,6 +59,7 @@ def fix_code_blocks(tokens):
         tokens.insert(idx, Token(Token.CLOSE_PARANTHESIS))
 
     def get_indent_level(tokens, index_start):
+        """retorna um inteiro que corresponde ao nivel de indentação"""
         while index_start < len(tokens):
             if tokens[index_start].type == Token.BREAK_LINE:
                 indent_level = 0
@@ -50,12 +73,14 @@ def fix_code_blocks(tokens):
         return indent_level
 
     def get_index_next_breakline(index_start, tokens):
+        """retorna o indice do proximo caractere de quebra de linha"""
         while index_start < len(tokens):
             if tokens[index_start].type == Token.BREAK_LINE:
                 return index_start
             index_start += 1
 
     def find_end_block(tokens, index_start, indent_level):
+        """retorna o indice do fim do bloco de codigo"""
         while True:
             index_breakline = get_index_next_breakline(index_start, tokens)
             if index_breakline is not None:
@@ -71,6 +96,11 @@ def fix_code_blocks(tokens):
                 return len(tokens)
 
     def fix_end(index_end, tokens):
+        """
+        usando o indice que representa o fim do bloco, aplica
+        a correção que consiste em adicionar um token de fechamento de
+        chave '}'
+        """
         if index_end != len(tokens):
             if tokens[index_end].type == Token.CLOSE_KEYS:
                 tokens.insert(index_end, Token(Token.BREAK_LINE))
@@ -85,22 +115,22 @@ def fix_code_blocks(tokens):
             tokens.append(Token(Token.BREAK_LINE))
             tokens.append(Token(Token.CLOSE_KEYS))
 
-    _index = 0
-    while _index < len(tokens):
-        if tokens[_index].type == Token.IDENTIFIER and \
-           (tokens[_index].value == 'if' or \
-            tokens[_index].value == 'else if' or \
-            tokens[_index].value == 'while'):
-            fix_header_block(_index, tokens)
-            indent_level = get_indent_level(tokens, _index)
-            index_end = find_end_block(tokens, _index, indent_level)
+    idx = 0
+    while idx < len(tokens):
+        if tokens[idx].type == Token.IDENTIFIER and \
+           (tokens[idx].value == 'if' or \
+            tokens[idx].value == 'else if' or \
+            tokens[idx].value == 'while'):
+            fix_header_block(idx, tokens)
+            indent_level = get_indent_level(tokens, idx)
+            index_end = find_end_block(tokens, idx, indent_level)
             fix_end(index_end, tokens)
 
-        elif tokens[_index].type == Token.IDENTIFIER and \
-             tokens[_index].value == 'else':
-            tokens[_index+1].type = Token.OPEN_KEYS
-            indent_level = get_indent_level(tokens, _index)
-            index_end = find_end_block(tokens, _index, indent_level)
+        elif tokens[idx].type == Token.IDENTIFIER and \
+             tokens[idx].value == 'else':
+            tokens[idx+1].type = Token.OPEN_KEYS
+            indent_level = get_indent_level(tokens, idx)
+            index_end = find_end_block(tokens, idx, indent_level)
             fix_end(index_end, tokens)
 
-        _index +=1
+        idx +=1
